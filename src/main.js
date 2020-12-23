dbc1732a4d695948cca92476554919dbc6930c05
yue.liu <yue.liu@cloudsdnet.com>
2020-12-23 16:19:58 +0800

    引入mockjs

--- src/main.js
+++ src/main.js
@@ -2,13 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+//引入路由
 import router from './router'
 
+//引入VueX
 import store from "./store"
+//引入i18n国际化
 import i18n from "./lang"
+//引入ElementUi
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
+//引入mockjs
+require('./mock/api.js');
 
 
 
